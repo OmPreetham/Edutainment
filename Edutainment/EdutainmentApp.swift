6e945eb4d1bd3df7d2aac3e3f5e4da830a7f5d34
Om Preetham <ompreethambandi@gmail.com>
2024-05-21 02:57:48 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Om Preetham Bandi on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
